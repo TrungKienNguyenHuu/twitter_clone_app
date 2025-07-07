1494f5ad7ff12cc5e5bbcdc91259d90289198cd2
Nguyen Huu Trung Kien <nguyenhuutrungkien1812@gmail.com>
2025-07-07 15:32:31 +0700

    add firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:twitter_clone_app/signup.dart';
+import 'package:twitter_clone_app/pages/signin.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
@@ -15,125 +20,16 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+        //colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+        brightness: Brightness.dark,
+        appBarTheme: AppBarTheme(
+          centerTitle: true,
+          toolbarHeight: 100,
+          shadowColor: Colors.yellow,
+          ),
       ),
-      home: const MyHomePage(title: 'Twitter Clone'),
+      home: const SignIn(title: 'Twitter Clone'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final GlobalKey<FormState> _signInKey = GlobalKey();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  final RegExp emailValid = RegExp(
-    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Form(
-        key: _signInKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget> [
-           const FaIcon(
-            FontAwesomeIcons.twitter,
-            color: Colors.blue,
-            size: 70,
-            ),
-            const Text(
-              "Log in to Twitter",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(15, 30, 15, 0),
-              padding: EdgeInsets.symmetric(vertical: 10),
-              decoration: BoxDecoration(
-                color: Colors.grey.shade200,
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                controller: _emailController,
-                decoration: InputDecoration(
-                  hintText: "Email your email",
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.symmetric(
-                    vertical: 15,
-                    horizontal: 20,
-                  )
-                ),
-                validator: (value){
-                  if(value == null || value.isEmpty){
-                    return "Please enter a email";
-                  }else if(!emailValid.hasMatch(value)){
-                    return 'Please enter a valid email';
-                  }
-                  return null;
-                },
-              
-              ),
-            ), //email
-            Container(
-              margin: EdgeInsets.all(15),
-              padding: EdgeInsets.symmetric(vertical: 10),
-              decoration: BoxDecoration(
-                color: Colors.grey.shade200,
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: TextFormField(
-                obscureText: true,
-                controller: _passwordController,
-                decoration: const InputDecoration(
-                  hintText: "Enter a Password",
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.symmetric(
-                    vertical: 15,
-                    horizontal: 20,
-                  )
-                ),
-                validator: (value){
-                  if(value == null || value.isEmpty){
-                    return "Please enter a password";
-                  }else if(value.length < 6){
-                    return 'Password must be at least 6 characters';
-                  }
-                  return null;
-                },
-              ),
-            ),
-             //password
-            Container(
-              width: 250,
-              decoration: BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.circular(30)),
-              child: TextButton(onPressed: (){
-                if(_signInKey.currentState!.validate()){
-                  debugPrint("Email: ${_emailController.text}");
-                  debugPrint("Password: ${_passwordController.text}");
-                }
-              }, child: Text("Log in", 
-                style: TextStyle(color: Colors.white, fontSize: 18),
-                )
-              ),
-            ),
-
-            TextButton(onPressed: (){
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => Signup()));
-              },
-              child: const Text("Don't have an account? Sign up here")
-              ),
-          ]
-        )
-      )
-    );
-  }
-}
